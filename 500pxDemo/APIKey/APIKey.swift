06db7cb56dafe15e1814aefa55884ac866400474
Yung Dai <yungchidai@gmail.com>
2019-02-10 11:00:41 -0500

    Added API Key file

---
+++ 500pxDemo/APIKey/APIKey.swift
@@ -0,0 +1,11 @@
+//
+//  APIKey.swift
+//  500pxDemo
+//
+//  Created by Yung Dai on 2019-02-10.
+//  Copyright © 2019 Yung Dai. All rights reserved.
+//
+
+import Foundation
+
+let APIKey = <# API Key String #>
